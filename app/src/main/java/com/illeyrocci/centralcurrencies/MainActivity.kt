fff576a00c8ae7a52562af897a9899678304174e
Illeyrocci <40275990+Illeyrocci@users.noreply.github.com>
2024-04-03 16:06:14 +0300

    just user interface

---
+++ app/src/main/java/com/illeyrocci/centralcurrencies/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.illeyrocci.centralcurrencies
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //paints status bar
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = getResources().getColor(R.color.primary, theme)
+
+
+    }
+}
